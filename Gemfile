fdf8a4631b3de2e37f823ae6fc57b57b58403be1
Ken Barber <ken@bob.sh>
2012-08-22 15:59:47 +0100

    Add tests for binary files

--- Gemfile
+++ Gemfile
@@ -4,10 +4,27 @@
 source "http://rubygems.org"
 
 gem 'data_mapper'
-gem 'dm-timestamps'
+#gem 'dm-timestamps'
 gem 'rack-parser', :require => 'rack/parser'
 
-group :development do
-  gem 'dm-sqlite-adapter'
-  gem 'dm-visualizer'
-end
+gem 'dm-postgres-adapter', :git => 'git://github.com/datamapper/dm-postgres-adapter'
+gem 'dm-sqlite-adapter', :git => 'git://github.com/datamapper/dm-sqlite-adapter'
+gem 'dm-do-adapter', :git => 'git://github.com/datamapper/dm-do-adapter'
+gem 'dm-core', :git => 'git://github.com/datamapper/dm-core'
+gem 'dm-validations', :git => 'git://github.com/datamapper/dm-validations'
+gem 'dm-types', :git => 'git://github.com/datamapper/dm-types'
+gem 'dm-migrations', :git => 'git://github.com/datamapper/dm-migrations'
+gem 'dm-aggregates', :git => 'git://github.com/datamapper/dm-aggregates'
+gem 'do_postgres', :git => 'git://github.com/datamapper/do'
+gem 'pg'
+
+=begin
+gem 'dm-postgres-adapter'
+gem 'dm-do-adapter'
+gem 'dm-core'
+gem 'dm-validations'
+gem 'dm-types'
+gem 'dm-migrations'
+gem 'dm-aggregates'
+gem 'do_postgres'
+=end
--- dmtest.rb
+++ dmtest.rb
@@ -3,40 +3,75 @@
 require 'bundler/setup'
 Bundler.require :default, :development
 
+Encoding.default_internal = "BINARY"
+
 DM = DataMapper
 
-class User
+class Release
   include DataMapper::Resource
 
-  has n, :mods
-
+  belongs_to :module, :model => 'Mod'
   property :id, Serial
+  property :version, String
+  property :file_data, Binary, :length => 16 * 1024 * 1024, :required => false
+
+=begin
+  def file_data=(data)
+    super(PGconn.escape_bytea(data))
+  end
+
+  def file_data
+    if fd = super
+      PGconn.unescape_bytea(fd)
+    end
+  end
+=end
 end
 
 class Mod
   include DataMapper::Resource
 
   belongs_to :user
-  has n, :releases
-
   property :id, Serial
 end
 
-class Release
+class User
   include DataMapper::Resource
-
-  belongs_to :mod
 
   property :id, Serial
 end
 
 UAPWM = DM::NamingConventions::Resource::UnderscoredAndPluralizedWithoutModule
-DM.setup(:default, 'sqlite3::memory:')
+DM::Logger.new(STDOUT, :debug)
+#DM.setup(:default, 'sqlite3:///Users/ken/Development/dmtest/db.sqlite3')
+DM.setup(:default, 'postgres://dmtest:dmtest@localhost/dmtest')
 DM.repository(:default).adapter.resource_naming_convention = UAPWM
 DM::Model.raise_on_save_failure = true
 DM.finalize
 DM.auto_migrate!
 
-user = User.create()
-mod = Mod.create(:user => user)
-release = Release.create(:mod => mod)
+begin
+  user = User.create()
+  mod = Mod.create(:user => user)
+  release = Release.create(:module => mod, :version => '1.1')
+
+  file = File.open("/bin/ls", "rb")
+  file_data = file.read
+  puts file_data.length
+  file_data = PGconn.escape_bytea(file_data)
+  DM.repository(:default).adapter.execute("update releases set file_data = ? where id = #{release.id}", file_data)
+  #DM.repository(:default).adapter.execute("update releases set file_data = '#{file_data}' where id = #{release.id}")
+  #release.file_data = file_data
+  #release.save
+
+  new_file = Release.first(:module => mod).file_data
+  new_file = PGconn.unescape_bytea(new_file)
+  puts new_file.length
+  File.unlink("ls")
+  filenew = File.open("ls", "wb")
+  filenew.write(new_file)
+
+rescue DataMapper::SaveFailureError => e
+  puts "Class: #{e.resource}"
+  puts e.resource.errors.full_messages.join(",")
+end
